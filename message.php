c615a224419805a2ce09982c1c89a2461c98ada2
asafonov <me@asafonov.org>
2026-08-08 08:55:50 +0000

    fix content-type in sendPhoto

--- message.php
+++ message.php
@@ -101,10 +101,7 @@ function sendPhotoWithRetry ($msg) {
 
   $httpOptions = [
     'method' => 'POST',
-    'header' => [
-      'Content-Type' => 'multipart/form-data; boundary=' . $boundary,
-      'Content-Length' => strlen($data)
-    ],
+    'header' => 'Content-Type: multipart/form-data; boundary=' . $boundary . '\r\nContent-Length:' . strlen($data) . "\r\n"
     'content' => $data
   ];
   $url = 'https://api.telegram.org/bot' . TOKEN . '/sendPhoto';
